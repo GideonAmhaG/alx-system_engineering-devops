5d9593bd9ee8cf00cb77004a7d050168c68a4af2
GideonAmhaG <gideonamha@gmail.com>
2023-05-23 12:53:26 +0300

    1st commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your Python script to export
+ data in the JSON format"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    _id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": _id}).json()
+    with open("{}.json".format(_id), "w", newline="") as jsonfile:
+        json.dump({_id: [{
+            "task": a.get("title"),
+            "completed": a.get("completed"),
+            "username": username}
+            for a in todos]}, jsonfile)
